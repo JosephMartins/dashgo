7917f9e132651f41851f58eb4af2365dc3cbbb96
Jose Martins <jose.melo@doutorhoje.com>
2021-05-24 01:40:45 -0300

    [UPDATE] Remocao de alguns componentes do header na wide mobile

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,21 +1,28 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps{
+  showProfileData: boolean;
+}
+
+export function Profile({showProfileData}: ProfileProps){
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Bianca Martins</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          Krasota.b@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+            
+          <Text>Joseph Martins</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            martins.jose@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar 
         size="md" 
-        name="Bianca Martins" 
+        name="Joseph Martins" 
         src="https://avatars.githubusercontent.com/u/44592740?v=4"
       />
     </Flex>  
